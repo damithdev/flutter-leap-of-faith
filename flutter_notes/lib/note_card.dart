b6ac2f1787bc85e67f2e37d5e12400357b9761a9
sendt <damith.dev@gmail.com>
2020-09-14 16:33:43 +0530

    Card UI Body

--- flutter_notes/lib/note_card.dart
+++ flutter_notes/lib/note_card.dart
@@ -1,17 +1,23 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/constants.dart';
 
-class NoteCard extends StatelessWidget {
+class NoteCard extends StatelessWidget {  
+
+  final double _card_height = 130;
+
   String note = StringConstatnts.sample_text;
-  DateTime time;
+  DateTime time = DateTime.now();
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(10),
-        height: 200,
+        height: _card_height,
         width: double.maxFinite,
         child: Card(
           elevation: 5,
@@ -20,7 +26,15 @@ class NoteCard extends StatelessWidget {
             child: Column(
               children: [
                 Container(
-                  child: Text(note),
+                  height: _card_height - 70,
+                  child: Text(note,
+                  maxLines: 3,
+                  textAlign: TextAlign.justify,
+                  overflow: TextOverflow.ellipsis,),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Text("Last Updated : "+time.toString()),
                 )
               ],
             ),
