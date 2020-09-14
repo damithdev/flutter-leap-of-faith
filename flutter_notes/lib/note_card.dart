18d8a5bb5d18db351a8d9a3fe063e8337ce199fa
sendt <damith.dev@gmail.com>
2020-09-14 16:41:33 +0530

    Card Styling + Google Fonts

--- flutter_notes/lib/note_card.dart
+++ flutter_notes/lib/note_card.dart
@@ -3,17 +3,18 @@ import 'dart:html';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class NoteCard extends StatelessWidget {  
-
+class NoteCard extends StatelessWidget {
   final double _card_height = 130;
 
   String note = StringConstatnts.sample_text;
   DateTime time = DateTime.now();
 
-
   @override
   Widget build(BuildContext context) {
+    final TextStyle cardText = Theme.of(context).textTheme.caption;
+
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(10),
@@ -27,14 +28,18 @@ class NoteCard extends StatelessWidget {
               children: [
                 Container(
                   height: _card_height - 70,
-                  child: Text(note,
-                  maxLines: 3,
-                  textAlign: TextAlign.justify,
-                  overflow: TextOverflow.ellipsis,),
+                  child: Text(
+                    note,
+                    maxLines: 3,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.roboto(),
+                  ),
                 ),
                 Container(
                   alignment: Alignment.centerRight,
-                  child: Text("Last Updated : "+time.toString()),
+                  child: Text("Last Updated : " + time.toString(),
+                  style: GoogleFonts.roboto(fontStyle: FontStyle.italic,color: Colors.grey),),
                 )
               ],
             ),
